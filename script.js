d3df1de829ff712d6b8f5b6714a98087425b8422
ibeO-GH <ibe.okorafor@gmail.com>
2024-08-26 14:07:41 +0100

    updates on all

--- script.js
+++ script.js
@@ -14,18 +14,4 @@ $(document).ready(function () {
       $(".top").hide();
     }
   });
-
-  // smooth scrolling
-
-  $('a[href*="#"]').on("click", function (e) {
-    e.preventDefault();
-
-    $("html, body").animate(
-      {
-        scrollTop: $($(this).attr("href")).offset().top,
-      },
-      500,
-      "linear"
-    );
-  });
 });
