2447203ba2e1bd6ae6c8a00c3788ff1486bf12d8
ibeO-GH <ibe.okorafor@gmail.com>
2024-09-01 23:06:06 +0100

    updates

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ $(document).ready(function () {
     }
   });
 });
+
+document
+  .getElementById("registration-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const subject = document.getElementById("subject").value.trim();
+    const textarea = document.getElementById("textarea").value.trim();
+    let message = "";
+
+    switch (true) {
+      case !name:
+        message = "Name is required.";
+        break;
+      case !email:
+        message = "Email is required.";
+        break;
+      case !subject:
+        message = "Subject is required.";
+        break;
+      case !textarea:
+        message = "Message is required.";
+        break;
+      default:
+        message = "Form submitted successfully!";
+    }
+
+    document.getElementById("validation-message").innerHTML = message;
+  });
+
+const validationMessage = document.getElementById("validation-message");
+validationMessage.style.fontSize = "3rem";
+validationMessage.style.color = "var(--main-color)";
+validationMessage.style.textAlign = "center";
